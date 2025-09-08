76bab4754dd78536bf0ac4f951b00029163104b4
safrongrisa9-netizen <safrongrisa9@gmail.com>
2025-09-08 15:42:30 +0300

    utils.js

---
+++ scripts/utils.js
@@ -0,0 +1,21 @@
+// Вспомогательные функции
+function formatTime(timestamp) {
+    const date = new Date(timestamp);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
+
+function generateId() {
+    return Math.random().toString(36).substr(2, 9);
+}
+
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
